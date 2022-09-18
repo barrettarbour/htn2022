a72c96ecd55e436bb67dee98eb29fcfe6ce78dc1
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2022-09-18 02:49:24 -0400

    use rear camera on mobile and fix bg colors

--- client/src/components/Ingredients/Ingredients.js
+++ client/src/components/Ingredients/Ingredients.js
@@ -1,136 +1,135 @@
-import React, { useState, useRef, useEffect } from "react";
-import Tesseract from "tesseract.js";
+import React, { useState, useRef, useEffect } from 'react';
+import Tesseract from 'tesseract.js';
 
 function preprocessImage(canvas) {
-  const image = canvas
-    .getContext("2d")
-    .getImageData(0, 0, canvas.width, canvas.height);
-  thresholdFilter(image.data, 0.5);
-  return image;
+	const image = canvas
+		.getContext('2d')
+		.getImageData(0, 0, canvas.width, canvas.height);
+	thresholdFilter(image.data, 0.5);
+	return image;
 }
 
 // from https://github.com/processing/p5.js/blob/main/src/image/filters.js
 function thresholdFilter(pixels, level) {
-  if (level === undefined) {
-    level = 0.5;
-  }
-  const thresh = Math.floor(level * 255);
-  for (let i = 0; i < pixels.length; i += 4) {
-    const red = pixels[i];
-    const green = pixels[i + 1];
-    const blue = pixels[i + 2];
+	if (level === undefined) {
+		level = 0.5;
+	}
+	const thresh = Math.floor(level * 255);
+	for (let i = 0; i < pixels.length; i += 4) {
+		const red = pixels[i];
+		const green = pixels[i + 1];
+		const blue = pixels[i + 2];
 
-    const gray = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
-    let value;
-    if (gray >= thresh) {
-      value = 255;
-    } else {
-      value = 0;
-    }
-    pixels[i] = pixels[i + 1] = pixels[i + 2] = value;
-  }
+		const gray = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+		let value;
+		if (gray >= thresh) {
+			value = 255;
+		} else {
+			value = 0;
+		}
+		pixels[i] = pixels[i + 1] = pixels[i + 2] = value;
+	}
 }
 
 const Ingredients = ({ handleSubmit }) => {
-  const videoRef = useRef(null);
-  const photoRef = useRef(null);
+	document.body.style.backgroundColor = '#efbed5';
+	const videoRef = useRef(null);
+	const photoRef = useRef(null);
 
-  const [hasPhoto, setHasPhoto] = useState(false);
+	const [hasPhoto, setHasPhoto] = useState(false);
 
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: { width: 1920, height: 1080 },
-      })
-      .then((stream) => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+	const getVideo = () => {
+		navigator.mediaDevices
+			.getUserMedia({ video: { facingMode: 'environment' } })
+			.then((stream) => {
+				let video = videoRef.current;
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
+	useEffect(() => {
+		getVideo();
+	}, [videoRef]);
 
-  const takePhoto = () => {
-    const width = 414;
-    const height = width / (16 / 9);
+	const takePhoto = () => {
+		const width = 414;
+		const height = width / (16 / 9);
 
-    let video = videoRef.current;
-    let photo = photoRef.current;
+		let video = videoRef.current;
+		let photo = photoRef.current;
 
-    photo.width = width;
-    photo.height = height;
+		photo.width = width;
+		photo.height = height;
 
-    //const canvas = photoRef.current;
-    let ctx = photo.getContext("2d");
-    //ctx.putImageData(preprocessImage(canvas), 0, 0);
-    //const dataUrl = canvas.toDataURL("image/jpeg");
+		//const canvas = photoRef.current;
+		let ctx = photo.getContext('2d');
+		//ctx.putImageData(preprocessImage(canvas), 0, 0);
+		//const dataUrl = canvas.toDataURL("image/jpeg");
 
-    ctx.drawImage(video, 0, 0, width, height);
-    setHasPhoto(true);
-  };
+		ctx.drawImage(video, 0, 0, width, height);
+		setHasPhoto(true);
+	};
 
-  const handleClick = () => {
-    let photo = photoRef.current;
-    const dataUrl = photo.toDataURL();
-    console.log(dataUrl);
+	const handleClick = () => {
+		let photo = photoRef.current;
+		const dataUrl = photo.toDataURL();
+		console.log(dataUrl);
 
-    Tesseract.recognize(dataUrl, "eng", { logger: (m) => console.log(m) })
-      .catch((err) => {
-        console.error(err);
-      })
-      .then(({ data: { text } }) => {
-        console.log(text);
-        //setIngredients(text);
-        handleSubmit(text);
-      });
-  };
+		Tesseract.recognize(dataUrl, 'eng', { logger: (m) => console.log(m) })
+			.catch((err) => {
+				console.error(err);
+			})
+			.then(({ data: { text } }) => {
+				console.log(text);
+				//setIngredients(text);
+				handleSubmit(text);
+			});
+	};
 
-  const [ingredients, setIngredients] = useState("");
+	const [ingredients, setIngredients] = useState('');
 
-  const handleChange = (e) => {
-    setIngredients(e.target.value);
-  };
+	const handleChange = (e) => {
+		setIngredients(e.target.value);
+	};
 
-  return (
-    <div class="wrapper2">
-      <div class="padding">
-        <div classname="camera">
-          <video ref={videoRef}></video>
-          <button class="submitBtn" onClick={takePhoto}>
-            Take pic!
-          </button>
-        </div>
-        <div className={"result" + (hasPhoto ? "hasPhoto" : "")}>
-          <canvas ref={photoRef}></canvas>
-        </div>
-        <button onClick={handleClick}> Convert </button>
-        <form>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={ingredients}
-              onChange={handleChange}
-              name="name"
-            />
-          </label>
-          <input
-            type="submit"
-            value="Next"
-            onClick={() => {
-              handleSubmit(ingredients);
-            }}
-          />
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div class='wrapper2'>
+			<div class='padding'>
+				<div classname='camera'>
+					<video ref={videoRef}></video>
+					<button class='submitBtn' onClick={takePhoto}>
+						Take pic!
+					</button>
+				</div>
+				<div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+					<canvas ref={photoRef}></canvas>
+				</div>
+				<button onClick={handleClick}> Convert </button>
+				<form>
+					<label>
+						Name:
+						<input
+							type='text'
+							value={ingredients}
+							onChange={handleChange}
+							name='name'
+						/>
+					</label>
+					<input
+						type='submit'
+						value='Next'
+						onClick={() => {
+							handleSubmit(ingredients);
+						}}
+					/>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Ingredients;
--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,84 +1,93 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Results = ({ selectedChoices, ingredients }) => {
-  const API_URL = "http://localhost:3001";
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+	document.body.style.backgroundColor = '#86d3ea';
+	const API_URL = 'http://localhost:3001';
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(
-          `${API_URL}/api?ingredients=${ingredients}`
-        );
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        let actualData = await response.json();
-        setData(actualData);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setData(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await fetch(
+					`${API_URL}/api?ingredients=${ingredients}`
+				);
+				if (!response.ok) {
+					throw new Error(
+						`This is an HTTP error: The status is ${response.status}`
+					);
+				}
+				let actualData = await response.json();
+				setData(actualData);
+				setError(null);
+			} catch (err) {
+				setError(err.message);
+				setData(null);
+			} finally {
+				setLoading(false);
+			}
+		};
+		getData();
+	}, []);
 
-  return (
-    <div class="wrapper3">
-      <div class="padding">
-        <h1>Results</h1>
-        {loading && <div>A moment please...</div>}
-        {error && (
-          <div>{`There is a problem fetching the post data - ${error}`}</div>
-        )}
-        {selectedChoices.map((choice) => {
-          if (choice.value && data.dairyIngredients && choice.name == "dairy") {
-            return (
-              <div class="ingredBox">
-                <div>{choice.name}</div>
-                We found <b>{data.dairyIngredients.length}</b> ingredients
-                flagged as dairy!
-                {data.dairyIngredients.toString()}
-              </div>
-            );
-          } else if (
-            choice.value &&
-            data.addedSugarIngredients &&
-            choice.name == "added sugar"
-          ) {
-            return (
-              <p>
-                {choice.name}
-                We found {data.addedSugarIngredients.length} ingredients flagged
-                as added sugar!
-                {data.addedSugarIngredients.toString()}
-              </p>
-            );
-          } else if (
-            choice.value &&
-            data.shellfishIngredients &&
-            choice.name == "shellfish"
-          ) {
-            return (
-              <p>
-                {choice.name}
-                We found {data.shellfishIngredients.length} ingredients flagged
-                as shellfish!
-                {data.shellfishIngredients.toString()}
-              </p>
-            );
-          }
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div class='wrapper3'>
+			<div class='padding'>
+				<h1>Results</h1>
+				{loading && <div>A moment please...</div>}
+				{error && (
+					<div>{`There is a problem fetching the post data - ${error}`}</div>
+				)}
+				{selectedChoices.map((choice) => {
+					if (
+						choice.value &&
+						data.dairyIngredients &&
+						choice.name == 'dairy'
+					) {
+						return (
+							<div class='ingredBox'>
+								<div>{choice.name}</div>
+								We found <b>
+									{data.dairyIngredients.length}
+								</b>{' '}
+								ingredients flagged as dairy!
+								{data.dairyIngredients.toString()}
+							</div>
+						);
+					} else if (
+						choice.value &&
+						data.addedSugarIngredients &&
+						choice.name == 'added sugar'
+					) {
+						return (
+							<p>
+								{choice.name}
+								We found {
+									data.addedSugarIngredients.length
+								}{' '}
+								ingredients flagged as added sugar!
+								{data.addedSugarIngredients.toString()}
+							</p>
+						);
+					} else if (
+						choice.value &&
+						data.shellfishIngredients &&
+						choice.name == 'shellfish'
+					) {
+						return (
+							<p>
+								{choice.name}
+								We found {data.shellfishIngredients.length}{' '}
+								ingredients flagged as shellfish!
+								{data.shellfishIngredients.toString()}
+							</p>
+						);
+					}
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default Results;
